485ce4d736c153a36d8e3d2dcd27477b5b41140d
Abraham Haskins <abeisgreat@abeisgreat.com>
2014-06-03 14:25:43 -0700

    Clean up README

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
-Cannon
+Cannon.js
 ======
 
 Tackle RESTful data and beat it into a useful format!
 
 For example, load the first 2 pages of pictures on a subreddit:
 
-    var Cannon = require('../src/Cannon')
-    (new Cannon("http://www.reddit.com/r/AbandonedPorn/.json"))
+    var Cannon = require('cannon');
+    (new Cannon("http://www.reddit.com/r/Pics/.json"))
         .projectile("data.children.*.data.url")
         .reloader({after: "data.after"})
         .limit(2)
         .fetcher(function (current, limit) {
+            // Called on each HTTP load
             console.log('Fetching ' + current + ' of ' + limit);
         })
         .rapidfire(function (projectile, cannon) {
+            // Called once for each link in the subreddit
             console.log(projectile);
         });
 
--- tests/node-test.js
+++ tests/node-test.js
@@ -1,6 +1,13 @@
-var Cannon = require('../src/Cannon')
-(new Cannon("http://www.reddit.com/r/AbandonedPorn/.json")).projectile("data.children.*.data.url").reloader({after: "data.after"}).limit(2).fetcher(function (current, limit) {
-    console.log('Fetching ' + current + ' of ' + limit);
-}).rapidfire(function (projectile, cannon) {
-    console.log(projectile);
-});
+var Cannon = require('../src/Cannon');
+(new Cannon("http://www.reddit.com/r/Pics/.json"))
+    .projectile("data.children.*.data.url")
+    .reloader({after: "data.after"})
+    .limit(2)
+    .fetcher(function (current, limit) {
+        // Called on each HTTP load
+        console.log('Fetching ' + current + ' of ' + limit);
+    })
+    .rapidfire(function (projectile, cannon) {
+        // Called once for each link in the subreddit
+        console.log(projectile);
+    });
